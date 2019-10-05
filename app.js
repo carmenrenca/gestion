3a32d9842c772d8a5671a73196a496e5802f53f2
carmen <carmen@carmenweb.com>
2019-10-05 20:48:32 +0200

    segundo commit

--- app.js
+++ app.js
@@ -120,11 +120,14 @@ function editaruser(id,nombre, apellido, direccion,telefono, email, fecha, dni){
 	document.getElementById('fecha').value = fecha;
 	document.getElementById('dni').value = dni;
 	//obtenemos el boton y  vamos a cambiarle el nombre por 'editar'
-	var boton = document.getElementById('boton');
-	boton.innerHTML='Editar';
+	var a = document.getElementById('boton');
+	var b = document.getElementById('botonedit');
+	a.style.display='none';
+	b.style.display = '';
 	//limpiar formulario
 	
-	boton.onclick= function(){
+	b.onclick= function(){
+		console.log("entroo")
 		var washingtonRef = db.collection("clientes").doc(id);
 	
 var nombre=  document.getElementById('nombre').value;
@@ -152,8 +155,15 @@ var dni=	document.getElementById('dni').value;
 	document.getElementById('email').value = '';
 	document.getElementById('fecha').value = '';
 	document.getElementById('dni').value = '';
+		b.innerHTML='Guardar';
 
 	    console.log("Document successfully updated!");
+ $("#miModal").modal('hide');//ocultamos el modal
+
+  $('body').removeClass('modal-open');//eliminamos la clase del body para poder hacer scroll
+  $('.modal-backdrop').remove();//eliminamos el backdrop del modal
+a.style.display='';
+	b.style.display = 'none';
 	})
 	.catch(function(error) {
 	  
@@ -162,9 +172,206 @@ var dni=	document.getElementById('dni').value;
 	}
 	
 	}
+function cerrarmodal(){
+	document.getElementById('nombre').value ='';
+	document.getElementById('apellido').value = '';
+	document.getElementById('direccion').value = '';
+	document.getElementById('telefono').value = '';
+	document.getElementById('email').value = '';
+	document.getElementById('fecha').value = '';
+	document.getElementById('dni').value = '';
+		
+
+}
+
+function buscacliente(){
+
+	
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tb");
+   console.log(table);
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  
+}
+}
+
+
+//////////////////////////////////ARTICULOS\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
 
+//agregar articulo
+function guarda_articulos(){
+
+
+var nombre = document.getElementById('nombre').value;
+var precio = document.getElementById('precio').value;
+var stock = document.getElementById('stock').value;
+var descripcion = document.getElementById('descripcion').value;
+
+if(nombre==""){
+	alert("Tienes que poner un nombre");
+	return false;
+}else if(precio.length==""){
+	alert("Tienes que poner un precio al artículo");
+	return false;
+}else if(stock.lenght==""){
+	alert("Tienes que poner un numero de stock");
+	return false;
+
+}else if(descripcion.lenght==""){
+	alert("Debes de poner una descripción al artículo");
+	return false;
+}else{
+	db.collection("articulos").add({
+
+    nombre: nombre,
+    precio: precio,
+    stock: stock,
+    descripcion: descripcion
+   
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+}
+
+}
+	
+var tabla= document.getElementById('tablaarticulo');
+
+db.collection("articulos").onSnapshot((querySnapshot) => {
+	//limpiamos la tabla 
+
+	tabla.innerHTML='';
+
+    querySnapshot.forEach((doc) => {
+
+    	tabla.innerHTML +=`
+    	<tr>
+    		<td>${doc.id }  </td>
+    		<td>${doc.data().nombre }  </td>
+    		<td>${doc.data().precio}</td>
+    		<td>${doc.data().stock}</td>
+    			<td>${doc.data().descripcion}</td>
+    		
+    	      <td> <button type="button" class="btn btn-danger" onclick="elimina_articulos('${doc.id }')">Eliminar</button></td>
+    	       <td><button type="button" class="btn btn-info" onclick="editar_user('${doc.id }','${doc.data().nombre}','${doc.data().precio}','${doc.data().stock}','${doc.data().descripcion}')" data-toggle="modal" data-target="#miModalart">Editar</button></td>
+    	</tr>
+    	`
+    
+
+
+      
+    });
+
+});
 
 
 
+function busca_articulo(){
+
+	
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tableart");
+   console.log(table);
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  
+}
+}
+
+//borrar articulos
+function elimina_articulos(iduser){
+db.collection("articulos").doc(iduser).delete().then(function() {
+    console.log("Document successfully deleted!");
+}).catch(function(error) {
+    console.error("Error removing document: ", error);
+});
+
+
+}
+
+//editar articulos
+
+
+function editar_user(id,nombre, precio, stock,descripcion){
+//voy a pasar toda la informacion de esa fila a mi formulario
+	document.getElementById('nombre').value = nombre;
+	document.getElementById('precio').value = precio;
+	document.getElementById('stock').value = stock;
+	document.getElementById('descripcion').value = descripcion;
+
+	//obtenemos el boton y  vamos a cambiarle el nombre por 'editar'
+	var a = document.getElementById('boton');
+	var b = document.getElementById('botonedit');
+	a.style.display='none';
+	b.style.display = '';
+	//limpiar formulario
+	
+	b.onclick= function(){
+		console.log("entroo")
+		var washingtonRef = db.collection("articulos").doc(id);
+	
+var nombre=  document.getElementById('nombre').value;
+var precio=	document.getElementById('precio').value;
+var stock=	document.getElementById('stock').value;
+var descripcion=	document.getElementById('descripcion').value;
+
+
+	return washingtonRef.update({
+	 nombre: nombre,
+   precio: precio,
+    stock: stock,
+    descripcion: descripcion
+	})
+	.then(function() {
+			document.getElementById('nombre').value ='';
+	document.getElementById('precio').value = '';
+	document.getElementById('stock').value = '';
+	document.getElementById('descripcion').value = '';
+
+		b.innerHTML='Guardar';
+
+	    console.log("Document successfully updated!");
+ $("#miModalart").modal('hide');//ocultamos el modal
+
+  $('body').removeClass('modal-open');//eliminamos la clase del body para poder hacer scroll
+  $('.modal-backdrop').remove();//eliminamos el backdrop del modal
+a.style.display='';
+	b.style.display = 'none';
+	})
+	.catch(function(error) {
+	  
+	    console.error("Error updating document: ", error);
+	});
+	}
+	
+	}
